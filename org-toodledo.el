880b8b8c3b90ccd38d469672c0eb338674f55fc3
myuhe <yuhei.maeda@gmail.com>
2015-01-31 23:49:33 +0900

    Refactoring

--- org-toodledo.el
+++ org-toodledo.el
@@ -65,15 +65,11 @@
 ;;           (lambda ()
 ;;             (local-unset-key "\C-o")
 ;;             (local-set-key "\C-od" 'org-toodledo-mark-task-deleted)
-;;             (local-set-key "\C-os" 'org-toodledo-sync)
-;;             )
-;;           )
+;;             (local-set-key "\C-os" 'org-toodledo-sync)）
 ;;    (add-hook 'org-agenda-mode-hook
 ;;           (lambda ()
 ;;             (local-unset-key "\C-o")
-;;             (local-set-key "\C-od" 'org-toodledo-agenda-mark-task-deleted)
-;;             )
-;;           )
+;;             (local-set-key "\C-od" 'org-toodledo-agenda-mark-task-deleted)）
 ;;
 ;; 4. Install 2 patches for url-http.el (these are written for 23.3, but may
 ;;    work for other versions, if not patch manually, as the diffs are
