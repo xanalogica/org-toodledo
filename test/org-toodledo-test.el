c651367c9e8d06dacf73be51a0f7ab8995a0c557
myuhe <yuhei.maeda@gmail.com>
2015-02-01 11:59:23 +0900

    Fix test

--- test/org-toodledo-test.el
+++ test/org-toodledo-test.el
@@ -23,6 +23,28 @@
      :Parent-id:
      :END:")))
 
+(defun org-toodledo-parse-test ()
+  "Parse the org task at point and extract all toodledo related fields.  Return
+an alist of the task fields."
+   (save-excursion
+    (org-back-to-heading t)
+    (when (and (looking-at org-complex-heading-regexp)
+               (match-string 2)) ;; the TODO keyword
+      (org-toodledo-debug "org-toodledo-parse-current-task: %s"
+                          (match-string 0))
+      (let* (info
+             (status (match-string-no-properties 2))
+             (priority (match-string-no-properties 3))
+             (title (match-string-no-properties 4))
+             (tags-context (org-get-tags))
+             (id (org-entry-get (point) "ToodledoID"))
+             (deadline (org-entry-get nil "DEADLINE"))
+             (scheduled (org-entry-get nil "SCHEDULED"))
+             (closed (org-entry-get nil "CLOSED"))
+             (context "0")
+             tags)
+             status))))
+
 (ert-deftest org-toodledo-initialize-test ()
   (setq expected '(("duetime" . "0")
                    ("duedate" . "0")
@@ -42,5 +64,5 @@
                    ("priority" . "0")
                    ("note" . "")))
     (org-toodledo-test-setup-buffer "*test*")
-    (setq actual (org-toodledo-parse-current-task))
-    (should (equal expected actual)))
+    (setq actual (org-toodledo-parse-current-task)
+    (should (equal expected (org-toodledo-parse-test)))))
