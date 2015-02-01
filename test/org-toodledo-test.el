efead4b218e455d94fa6db1361f0d746e90817ab
myuhe <yuhei.maeda@gmail.com>
2015-02-01 13:00:04 +0900

    Fix test

--- test/org-toodledo-test.el
+++ test/org-toodledo-test.el
@@ -8,22 +8,28 @@
     (when (not (eq major-mode 'org-mode))
       (org-mode))
     (insert "* TASKS
-** TODO [#D] Test1            :@work:
+** WAITING [#D] Test2   :@work: 
+    :PROPERTIES:
+    :ToodledoID: 393773069  
+    :Hash:     3da632c2b88319569f648c35506cf0ba 
+    :Parent-id:
+    :END:
+** TODO [#D] Test1      :@work:
     DEADLINE: <2015-02-07 土 +1m>
     :PROPERTIES:
     :ToodledoID: 393655887
     :Hash:     ad0d9f84a6a204e051925805181ed137
     :Parent-id:
     :LAST_REPEAT: [2015-01-14 水 16:52]
-    :END:
-** WAITING [#D] Test2  :@work:  :PROPERTIES:   :ToodledoID: 393773069   :Hash:     3da632c2b88319569f648c35506cf0ba :Parent-id:   :END:")))
+    :END:")))
 
 (defun org-toodledo-parse-test ()
   "Parse the org task at point and extract all toodledo related fields.  Return
 an alist of the task fields."
   (save-excursion
     (org-back-to-heading t)
-      (looking-at org-complex-heading-regexp)))
+    (looking-at org-complex-heading-regexp)
+    (match-string-no-properties 2)))
 
 (ert-deftest org-toodledo-initialize-test ()
   (setq expected '(("duetime" . "0")
@@ -45,4 +51,4 @@ an alist of the task fields."
                    ("note" . "")))
     (org-toodledo-test-setup-buffer "*test*")
     (setq actual (org-toodledo-parse-test))
-    (should (equal t actual)))
+    (should (equal "TODO" actual)))
