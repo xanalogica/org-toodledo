846df86ebb85676f049f355e9a9e8702214c4a1a
myuhe <yuhei.maeda@gmail.com>
2015-02-01 11:29:39 +0900

    Fix test

--- test/org-toodledo-test.el
+++ test/org-toodledo-test.el
@@ -7,30 +7,32 @@
     (erase-buffer)
     (when (not (eq major-mode 'org-mode))
       (org-mode))
-    (insert "* TASKS\n
-** TODO [#D] Test1            :@work:\n
-    DEADLINE: <2015-02-07 土 +1m>\n
-    :PROPERTIES:\n
-    :ToodledoID: 393655887\n
-    :Hash:     ad0d9f84a6a204e051925805181ed137\n
-    :Parent-id:\n
-    :LAST_REPEAT: [2015-01-14 水 16:52]\n
-    :END:\n
-    :LOGBOOK:\n
-    - State \"DONE\"       from  \"TODO\"       [2015-01-14 水 16:52]\n
-    - State \"DONE\"       from \"TODO\"       [2014-12-18 木 14:39]\n
-    :LAST_REPEAT: [2014-12-18 木 14:39]\n
-    :END:\n
-** WAITING [#D] Test2  :@work:\n
-     :PROPERTIES:\n
-     :ToodledoID: 393773069\n
-     :Hash:     3da632c2b88319569f648c35506cf0ba\n
-     :Parent-id:\n
+    (insert "* TASKS
+** TODO [#D] Test1            :@work:
+    DEADLINE: <2015-02-07 土 +1m>
+    :PROPERTIES:
+    :ToodledoID: 393655887
+    :Hash:     ad0d9f84a6a204e051925805181ed137
+    :Parent-id:
+    :LAST_REPEAT: [2015-01-14 水 16:52]
+    :END:
+    :LOGBOOK:
+    - State \"DONE\"       from  \"TODO\"       [2015-01-14 水 16:52]
+    - State \"DONE\"       from \"TODO\"       [2014-12-18 木 14:39]
+    :LAST_REPEAT: [2014-12-18 木 14:39]
+    :END:
+** WAITING [#D] Test2  :@work:
+     :PROPERTIES:
+     :ToodledoID: 393773069
+     :Hash:     3da632c2b88319569f648c35506cf0ba
+     :Parent-id:
+     :END:
+     :LOGBOOK:
+     - State \"WAITING\"    from \"WAITING\"    [2015-01-23 金 15:50]
      :END:\n
-     :LOGBOOK:\n
-     - State \"WAITING\"    from \"WAITING\"    [2015-01-23 金 15:50]\n
-     :END:\n
-     - State \"WAITING\"    from \"TODO\"       [2015-01-23 金 15:50]\n")))
+     - State \"WAITING\"    from \"TODO\"       [2015-01-23 金 15:50]")))
+
+
 
 (ert-deftest org-toodledo-initialize-test ()
     (setq expected '(("duetime" . "0")
@@ -41,7 +43,7 @@
                      ("repeat" . "")
                      ("goal" . "0")
                      ("folder" . "0")
-                     ("id")
+                     ("id" . "393773069")
                      ("title" . "Test2")
                      ("length" . "0")
                      ("context" . "1200627")
@@ -52,4 +54,4 @@
                      ("note" . "- State \"WAITING\"    from \"TODO\"       [2015-01-23 金 15:50]")))
     (org-toodledo-test-setup-buffer "*test*")
     (setq actual (org-toodledo-parse-current-task))
-    (should (equal expected (buffer-string))))
+    (should (equal expected actual)))
