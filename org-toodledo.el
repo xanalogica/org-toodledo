e13ca414425276a5c6ce2059a3aa30f534763a43
myuhe <yuhei.maeda@gmail.com>
2015-02-01 12:09:02 +0900

    Tweak travis pref

--- org-toodledo.el
+++ org-toodledo.el
@@ -108,9 +108,7 @@
 (require 'url)
 (require 'url-http)
 (require 'org-agenda)
-(if (version< emacs-version "24.3")
-    (eval-when-compile (require 'cl-lib))
-  (require 'cl-lib))
+(require 'cl-lib)
 
 (declare-function org-columns-quit "org-colview.el")
 (declare-function org-toodledo-test "org-toodledo-test.el")
