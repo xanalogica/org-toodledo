09ddb5b0ebbdb65f87dd4594af9bd1cdf559512b
myuhe <yuhei.maeda@gmail.com>
2015-02-01 11:47:18 +0900

    Fix test

--- test/org-toodledo-test.el
+++ test/org-toodledo-test.el
@@ -16,42 +16,31 @@
     :Parent-id:
     :LAST_REPEAT: [2015-01-14 水 16:52]
     :END:
-    :LOGBOOK:
-    - State \"DONE\"       from  \"TODO\"       [2015-01-14 水 16:52]
-    - State \"DONE\"       from \"TODO\"       [2014-12-18 木 14:39]
-    :LAST_REPEAT: [2014-12-18 木 14:39]
-    :END:
 ** WAITING [#D] Test2  :@work:
      :PROPERTIES:
      :ToodledoID: 393773069
      :Hash:     3da632c2b88319569f648c35506cf0ba
      :Parent-id:
-     :END:
-     :LOGBOOK:
-     - State \"WAITING\"    from \"WAITING\"    [2015-01-23 金 15:50]
-     :END:\n
-     - State \"WAITING\"    from \"TODO\"       [2015-01-23 金 15:50]")))
-
-
+     :END:")))
 
 (ert-deftest org-toodledo-initialize-test ()
-    (setq expected '(("duetime" . "0")
-                     ("duedate" . "0")
-                     ("starttime" . "0")
-                     ("startdate" . "0")
-                     ("repeatfrom" . "0")
-                     ("repeat" . "")
-                     ("goal" . "0")
-                     ("folder" . "0")
-                     ("id" . "393773069")
-                     ("title" . "Test2")
-                     ("length" . "0")
-                     ("context" . "1200627")
-                     ("tag" . "")
-                     ("completed" . "0")
-                     ("status" . "5")
-                     ("priority" . "0")
-                     ("note" . "- State \"WAITING\"    from \"TODO\"       [2015-01-23 金 15:50]")))
+  (setq expected '(("duetime" . "0")
+                   ("duedate" . "0")
+                   ("starttime" . "0")
+                   ("startdate" . "0")
+                   ("repeatfrom" . "0")
+                   ("repeat" . "")
+                   ("goal" . "0")
+                   ("folder" . "0")
+                   ("id" . "393773069")
+                   ("title" . "Test2")
+                   ("length" . "0")
+                   ("context" . "1200627")
+                   ("tag" . "")
+                   ("completed" . "0")
+                   ("status" . "5")
+                   ("priority" . "0")
+                   ("note" . "")))
     (org-toodledo-test-setup-buffer "*test*")
     (setq actual (org-toodledo-parse-current-task))
-    (should (equal expected org-complex-heading-regexp)))
+    (should (equal expected actual)))
