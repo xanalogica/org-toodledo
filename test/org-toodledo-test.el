1dfeeeceef2adf6356070b7cca33005a85dc9cf2
myuhe <yuhei.maeda@gmail.com>
2015-02-05 20:57:41 +0900

    * test/org-toodledo-test.el: Add test case

--- test/org-toodledo-test.el
+++ test/org-toodledo-test.el
@@ -25,19 +25,18 @@
     :Hash:     ad0d9f84a6a204e051925805181ed137
     :Parent-id:
     :LAST_REPEAT: [2015-01-14 水 16:52]
-    :END:")))
+    :END:
+    This is note.")))
 
-(defun org-toodledo-parse-test ()
-  "Parse the org task at point and extract all toodledo related fields.  Return
-an alist of the task fields."
-  (save-excursion
-    (org-back-to-heading t)
-    (looking-at org-complex-heading-regexp)
-    (match-string-no-properties 2)))
+(ert-deftest org-toodledo-entry-note-test ()
+  (setq expected "This is note.")
+    (org-toodledo-test-setup-buffer "*test*")
+    (setq actual (org-toodledo-entry-note)))
 
 (ert-deftest org-toodledo-parse-test ()
-  (setq org-toodledo-userid   (getenv "ToodledoID")
-        org-toodledo-password (getenv "ToodledoPass"))
+  (when (string= (system-name) "myuhe")
+    (setq org-toodledo-userid   (getenv "ToodledoID")
+          org-toodledo-password (getenv "ToodledoPass")))
   (setq expected '(("repeatfrom" . "0")
                    ("repeat" . "Every 1 month")
                    ("duedate" . "1423267200")
@@ -54,7 +53,8 @@ an alist of the task fields."
                    ("completed" . "0")
                    ("status" . "2")
                    ("priority" . "0")
-                   ("note" . "")))
-    (org-toodledo-test-setup-buffer "*test*")
-    (setq actual (org-toodledo-parse-current-task))
-    (should (equal expected actual)))
+                   ("note" . "This is note.")))
+  (org-toodledo-test-setup-buffer "*test*")
+  (setq actual (org-toodledo-parse-current-task))
+  (should (equal expected actual)))
+
